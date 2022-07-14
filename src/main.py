61c11b3e5f73eee92de7bc06bc8e2e301ceacdda
Mateus Marinho <mateus.marinho@hotmail.com.br>
2022-07-14 13:32:43 -0300

    :sparkles: feat - add products post and get

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-from mimetypes import init
-from fastapi import FastAPI
+from os import stat
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+
 from infra.app.config.database import init_db, get_session
-
-
 from infra.app.models.models import *
 
 app = FastAPI()
@@ -26,11 +25,27 @@ def start():
     init_db()
 
 
-@app.post("/products")
-def create_product(product: ProductBase):
-    return {"Msg": product}
+# Product Routes
+@app.post("/products", response_model=ProductRead, status_code=status.HTTP_201_CREATED)
+def create_product(*, session: Session = Depends(get_session), product: ProductCreate):
+    db_product = Product.from_orm(product)
+    session.add(db_product)
+    session.commit()
+    session.refresh(db_product)
+    return db_product
 
 
-@app.get("/products")
-def get_products():
-    return {"Msg": "Tá aqui ó"}
+@app.get("/products", response_model=List[ProductRead], status_code=status.HTTP_200_OK)
+def read_products(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, lte=100),
+):
+    products = session.exec(select(Product).offset(offset).limit(limit)).all()
+    return products
+
+
+# User Routes
+
+# Order Routes
